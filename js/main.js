17ade5208bd13a5808834955738545db4ac89ca5
Michelle Hoelzer <mhoelzer@pio.carrollu.edu>
2018-03-05 15:11:57 -0500

    did some wstyling

--- js/main.js
+++ js/main.js
@@ -1,3 +1,28 @@
 /* General jQuery & JS stuff goes here. */
 
 console.log("A starter template created by Eleven Fifty Academy under MIT Licensing.");
+$(document).ready(function() {
+        
+    /* affix the navbar after scroll below header */
+    $('#nav').affix({
+    offset: {
+    top: $('header').height()-$('#nav').height()
+    }
+    });    
+
+    /* highlight the top nav as scrolling occurs */
+    $('body').scrollspy({ target: '#nav' })
+
+    /* smooth scrolling for scroll to top */
+    $('.scroll-top').click(function(){
+    $('body,html').animate({scrollTop:0},1000);
+    })
+
+    /* smooth scrolling for nav sections */
+    $('#nav .navbar-nav li>a').click(function(){
+    var link = $(this).attr('href');
+    var posi = $(link).offset().top;
+    $('body,html').animate({scrollTop:posi},700);
+    });
+});
+
